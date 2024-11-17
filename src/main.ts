922a54ba94246bfc1dbc3f2cb6fb605fddd048bb
Quentin AUBRY <29443413+kuentinozaure@users.noreply.github.com>
2024-11-17 03:30:12 -0500

    chore: improve the limit of payload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ async function bootstrap() {
   );
 
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
